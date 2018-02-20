adb4b8a32319116cc02c9063e8b10b6c7d4bda59
R .M.K.CET <rmkcet@rmkcets-imac-7.local>
2018-02-20 11:04:52 +0530

    Initial Commit

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  chat_app
+//
+//  Created by R.M.K.CET on 19/02/18.
+//  Copyright © 2018 R.M.K.C.E.T. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <JSQMessagesViewController/JSQMessages.h>
+
+
+#endif /* Bridging_Header_h */
---
+++ chat_app/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  chat_app
+//
+//  Created by R.M.K.CET on 19/02/18.
+//  Copyright © 2018 R.M.K.C.E.T. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Constants
+{
+    
+    struct refs
+    {
+        static let databaseRoot = FIRDatabase.database().reference()
+        static let databaseChats = databaseRoot.child("chats")
+    }
+}
